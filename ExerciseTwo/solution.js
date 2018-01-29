48fad43bc5f0a5bb1650f990d96c4ee12f77d1b9
maheshhp <hpmahesh73@gmail.com>
2018-01-29 19:35:09 +0530

    Exercise Two: Verified exercise

--- ExerciseTwo/solution.js
+++ ExerciseTwo/solution.js
@@ -1,7 +1,8 @@
 require('es6-promise');
 
 let promise = new Promise(((fulfill, reject) => {
-
+  setTimeout(() => fulfill('FULFILLED!'), 300);
 }));
 
+// promise.then(console.log);
 module.exports = promise;
--- ExerciseTwo/solution.test.js
+++ ExerciseTwo/solution.test.js
@@ -1,9 +1,5 @@
 const promiseToPrint = require('./solution');
 
-global.console = {
-  log: jest.fn(),
-};
-
 describe('Tests for checking the string logged to the console', () => {
   test('Verify FULFILLED! is printed to the console', () => {
     expect.assertions(1);
