d19d72844bfd6062ee3c5133a7691c331939c56c
maheshhp <hpmahesh73@gmail.com>
2018-01-30 12:27:05 +0530

    Exercise Five: Verified exercise

--- ExerciseFive/solution.js
+++ ExerciseFive/solution.js
@@ -1,13 +1,14 @@
 require('es6-promise');
 
 let multiPromise = new Promise(((resolve, reject) => {
-
+  resolve('PROMISE VALUE');
 }));
 
 let onResolve = () => {
-
+  console.log('MAIN PROGRAM');
 };
 
-// multiPromise.then(console.log, onResolve);
+// multiPromise.then(console.log);
+// onResolve();
 
 module.exports = { multiPromise, onResolve };
