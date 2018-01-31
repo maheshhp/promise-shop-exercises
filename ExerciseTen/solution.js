f7ac7f14ea93f06d0c2008ba9e625f23cca309b8
maheshhp <hpmahesh73@gmail.com>
2018-01-31 11:28:51 +0530

    Exercise Ten - iterate and promiseResolver tests pass

--- ExerciseTen/solution.js
+++ ExerciseTen/solution.js
@@ -2,7 +2,7 @@ let alwaysThrows = () => {
   throw new Error('OH NOES');
 };
 
-let iterate = (argOne) => { };
+let iterate = (argOne) => { console.log(argOne); return argOne + 1; };
 
 let promiseResolver = Promise.resolve(iterate(1));
 
