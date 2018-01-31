be3b2511fd1f5f546dae6e38df0b8db7f326d4b1
maheshhp <hpmahesh73@gmail.com>
2018-01-31 11:30:50 +0530

    Exercise Ten - exercise verified

--- ExerciseTen/solution.js
+++ ExerciseTen/solution.js
@@ -6,5 +6,15 @@ let iterate = (argOne) => { console.log(argOne); return argOne + 1; };
 
 let promiseResolver = Promise.resolve(iterate(1));
 
+promiseResolver.then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(alwaysThrows)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(iterate)
+  .then(null, error => console.log(error.message));
 
 module.exports = { alwaysThrows, iterate, promiseResolver };
