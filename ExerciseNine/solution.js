a6deb529d1839da5777fc291363cc4e6d1efebfa
maheshhp <hpmahesh73@gmail.com>
2018-01-31 09:17:04 +0530

    Exercise Nine: Tests passing

--- ExerciseNine/solution.js
+++ ExerciseNine/solution.js
@@ -1,4 +1,9 @@
-let parsePromised = new Promise((resolve, reject) => {
-
+let parsePromised = jsonArg => new Promise((resolve, reject) => {
+  try {
+    console.log(jsonArg);
+    resolve(JSON.parse(jsonArg));
+  } catch (e) {
+    reject(new Error('Parse error'));
+  }
 });
 module.exports = { parsePromised };
--- ExerciseNine/solution.test.js
+++ ExerciseNine/solution.test.js
@@ -1,14 +1,14 @@
 const { parsePromised } = require('./solution');
 
+
 describe('Test for checking the parsed JSON and error logged by the promise', () => {
   test('Verify parsePromised resolves to the JSON supplied being parsed', () => {
-    process.argv[2] = { hello: 'world' };
+    let validJson = JSON.stringify({ name: 'John', age: 31, city: 'New York' });
     expect.assertions(1);
-    return expect(parsePromised).resolves.toBe(JSON.parse({ hello: 'world' }));
+    return expect(parsePromised(validJson)).resolves.toEqual(JSON.parse(validJson));
   });
   test('Verify parsePromised returns Error on invalid JSON as input', () => {
-    process.argv[2] = 'Helo World';
     expect.assertions(1);
-    return expect(parsePromised).rejects.toEqual(new Error('Parse error'));
+    return expect(parsePromised('Hello World')).rejects.toEqual(new Error('Parse error'));
   });
 });
